4d645b3fb0a815ee404664d8ec53c6406768a3d5
Eoin Davey <eoind@vey.ie>
2017-02-12 16:38:16 +0000

    Better week 1

--- w1.cpp
+++ w1.cpp
@@ -6,9 +6,9 @@ int c[256];
 
 int main(){
     std::string s;getline(std::cin,s);
-    for(int i =0; i < s.length(); c[s[i]]++, i++)
+    for(int i =0; i < s.length(); c[s[i++]]++)
         for(int j = 0; j < 8; j++)
-            printf(((j==7)?"%d ":"%d"),(((s[i] << j) & 128)!=0));
+            printf((((j==7)?((i==s.length()-1)?"%d\n":"%d "):"%d")),(((s[i] << j) & 128)!=0));
     for(int i = 0; i < 256; i++)
-        if(c[i]>0) printf(((c[i]==1)?"\n'%c' appeared %d time":"\n'%c' appeared %d times"), i, c[i]);
+        if(c[i]>0) printf(((c[i]==1)?"'%c' appeared %d time\n":"'%c' appeared %d times\n"), i, c[i]);
 }
