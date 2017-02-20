a5350064ce68101b77d11a1760138dbe2e193423
Eoin Davey <eoind@vey.ie>
2017-02-20 21:18:56 +0000

    w2 final

--- w2.cpp
+++ w2.cpp
@@ -3,6 +3,7 @@
 #include<string>
 #include<queue>
 #include<map>
+#include<algorithm>
 
 int c[256];
 std::map<char,std::string> d;
@@ -10,14 +11,12 @@ std::map<char,std::string> d;
 struct node{
     int data,occ;
     node * l, * r;
-    node(int d,char c, node * ll, node * rr){
-        occ=d, data=c, l=ll, r=rr;
-    }
+    node(int d,char c, node * ll, node * rr) : occ(d), data(c), l(ll), r(rr){}
 };
 
 bool comp (const node * lhs, const node * rhs){
-    if(lhs->occ==rhs->occ) return lhs->data > rhs->data;
-    return lhs->occ > rhs->occ;
+    if(lhs->occ != rhs->occ) return lhs->occ > rhs->occ;
+    return lhs->data > rhs->data;
 }
 
 void trav(node * root,std::string c){
@@ -28,21 +27,17 @@ void trav(node * root,std::string c){
 }
 
 int main(){
-    std::string s;getline(std::cin,s);
-    for(int i=0;i<s.length();c[s[i++]]++)
-        for(int j=0;j<7;j++)
-            printf((((j==6)?((i==s.length()-1)?"%d\n":"%d "):"%d")),((s[i]<<j)&64)!=0);
+    std::string s;
+    getline(std::cin,s);
+    for(int i=0;i<s.length();i++)c[s[i]]++;
     std::priority_queue<node *, std::vector<node *> ,decltype(&comp) > q(comp);
     for(int i=0;i<256;i++)
-        if(c[i]>0)printf(((c[i]==1)?"'%c' appeared %d time\n":"'%c' appeared %d times\n"),i,c[i]), q.push(new node(c[i],i,NULL,NULL));
-    node * f, * r, * n;
+        if(c[i]>0)q.push(new node(c[i],i,NULL,NULL));
+    node * f, * r;
     while(q.size()>1)
-        f = q.top(), q.pop(), r = q.top(), q.pop(), q.push(new node(f->occ + r->occ, 254,f,r));
-    node * h = q.top();
-    trav(h,"");
-    int sm = 0;
+        f = q.top(), q.pop(), r = q.top(), q.pop(), q.push(new node(f->occ + r->occ, ((f->data < r->data) ? f->data: r->data),f,r));
+    trav(q.top(),"");
     for(int i = 0; i < s.length(); i++)
-        std::cout << d[s[i]] << " ", sm+=d[s[i]].length();
-    std::cout << std::endl;
-    printf("Compressed size is %d bits / %d bits = %d%%\n",sm,s.length()*7,sm*100/(s.length()*7));
+        std::cout << d[s[i]];
+    std::cout << "\n";
 }
