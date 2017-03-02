3cf2665c953f207c3560d6eb0dbd7056f6bda5ad
Eoin Davey <eoind@vey.ie>
2017-03-02 14:00:12 +0000

    made use c-strings for speed

--- w4.cpp
+++ w4.cpp
@@ -2,6 +2,7 @@
 #include<fstream>
 #include<cstdio>
 #include<string>
+#include<cstring>
 #include<vector>
 #include<algorithm>
 
@@ -30,26 +31,27 @@ void dfs(node * r, int used, string * s){
                 (*s)+=input[i], dfs(r->adj[input[i]-'a'],(used|(1<<i)),s), s->erase(s->end()-1);
 }
 
-void insert(string * s, int i, node * r){
-    if(i==s->length())
+void insert(char * s, int i, node * r){
+    if(i==strlen(s))
         r->word = true;
-    else if(r->adj[s->at(i) - 'a']==NULL)
-        r->adj[s->at(i) - 'a']= new node(), insert(s,i+1,r->adj[s->at(i) - 'a']);
-    else insert(s,i+1,r->adj[s->at(i) - 'a']);
+    else if(r->adj[s[i] - 'a']==NULL)
+        r->adj[s[i] - 'a']= new node(), insert(s,i+1,r->adj[s[i] - 'a']);
+    else insert(s,i+1,r->adj[s[i] - 'a']);
 }
 
 int main(){
-    ifstream file ("dictionary.txt");
+    FILE * f = fopen("dictionary.txt","r");
     node * top = new node();
-    string l;
-    while(getline(file, l))
-        insert(&l,0,top);
+    char * buff = (char *) malloc(sizeof(char)*100);
+    while(fscanf(f, "%s",buff)==1)
+        insert(buff,0,top);
+    fclose(f);
+    free(buff);
     cin >> input;
     N = input.length();
-    l="";
+    string l;
     dfs(top, 0, &l);
     delete top;
-    file.close();
     sort(ls.begin(),ls.end(), [](string lhs, string rhs){if(lhs.length()==rhs.length())return lhs < rhs; else return lhs.length() > rhs.length(); });
     for(int i = 0;i < 10 && i < ls.size(); i++)
         cout << ls[i] << endl;
