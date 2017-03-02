fa3967a6c2fd1537b0a2f010b8ccebaabda5bed9
Eoin Davey <eoind@vey.ie>
2017-03-02 11:27:38 +0000

    update w4

--- w4.cpp
+++ w4.cpp
@@ -15,8 +15,7 @@ struct node{
     bool word, visited;
     node * adj[26];
     ~node(){
-        for(int i = 0; i < 26; i++)
-            if(adj[i]!=NULL) delete adj[i];
+        for(int i = 0; i < 26; i++) if(adj[i]!=NULL) delete adj[i];
     }
 };
 
@@ -27,21 +26,16 @@ void dfs(node * r, int used, string * s){
     r->visited = true;
     for(int i = 0; i < N; i++)
         if((used&(1<<i))==0)
-            if(r->adj[input[i]-'a']!=NULL){
-                (*s)+=input[i];
-                dfs(r->adj[input[i]-'a'],(used|(1<<i)),s);
-                s->erase(s->end()-1);
-            }
+            if(r->adj[input[i]-'a']!=NULL)
+                (*s)+=input[i], dfs(r->adj[input[i]-'a'],(used|(1<<i)),s), s->erase(s->end()-1);
 }
 
 void insert(string * s, int i, node * r){
-    if(i>=s->length()-1){
+    if(i==s->length())
         r->word = true;
-        return;
-    }
-    if(r->adj[s->at(i) - 'a']==NULL)
-        r->adj[s->at(i) - 'a']= new node();
-    insert(s,i+1,r->adj[s->at(i) - 'a']);
+    else if(r->adj[s->at(i) - 'a']==NULL)
+        r->adj[s->at(i) - 'a']= new node(), insert(s,i+1,r->adj[s->at(i) - 'a']);
+    else insert(s,i+1,r->adj[s->at(i) - 'a']);
 }
 
 int main(){
@@ -57,7 +51,7 @@ int main(){
     delete top;
     file.close();
     sort(ls.begin(),ls.end(), [](string lhs, string rhs){if(lhs.length()==rhs.length())return lhs < rhs; else return lhs.length() > rhs.length(); });
-    for(int i = 0; i < 10 && i < ls.size(); i++)
+    for(int i = 0;i < 10 && i < ls.size(); i++)
         cout << ls[i] << endl;
     return 0;
 }
