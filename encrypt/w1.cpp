f879dea738dc248a8e1422b80c3c2e15ea5edf64
Eoin Davey <eoind@vey.ie>
2017-03-06 15:34:51 +0000

    more w5

--- encrypt/w1.cpp
+++ encrypt/w1.cpp
@@ -9,24 +9,29 @@
 using namespace std;
 
 int c[256];
+char fileNames[][8] = {"eng.txt","ger.txt","pol.txt","ice.txt","spa.txt","swe.txt"};
 map<char, char> sym;
 
 int main(){
     ifstream f("encoded.txt");
     string s;
-    vector<pair<int, char> > ls;
+    vector<pair<double, char> > ls;
+    int tot=0;
     while(getline(f,s))
-        for(int i=0;i<s.length();c[s[i++]]++){}
+        for(int i=0;i<s.length();c[s[i++]]++, tot++){}
     for(int i=0;i<256;i++)
-        if(c[i]>0)ls.push_back(make_pair(c[i],i));
-    sort(ls.begin(), ls.end(), greater<pair<int, char> >());
+        if(c[i]>0)ls.push_back(make_pair(((double)c[i])/((double)tot),i));
+    sort(ls.begin(), ls.end(), greater<pair<double, char> >());
     cout << ls.size() << endl;
-    for(int i= 0; i < ls.size(); i++){
+
+    sym[ls[0].second] = ' ';
+    for(int i= 1; i < ls.size(); i++){
         if(getline(cin,s)){
             sym[ls[i].second] = s[0];
         } else {
             sym[ls[i].second] = ls[i].second;
         }
+        printf("%c : %lf -> %c\n", ls[i].second,ls[i].first,sym[ls[i].second]);
     }
     f.clear();
     f.seekg(0, ios::beg);
